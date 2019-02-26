0842c45fe95bf7bfea9da1c99fc58acf680be0e7
Eleanore Campbell <eleanorecampbell@Eleanortes-MacBook-Pro.local>
2019-02-26 09:48:04 +0100

    transfer over AO model and layers

---
+++ baltic/testing/alt_functions/ao_v2019_feb.R
@@ -0,0 +1,76 @@
+
+AO <- function(layers){
+
+  ## From code in 'functions.R AO' of v2015 BHI assessment
+  ## Revised to use multi-year framework, incorporating scenario_data_years
+  ## Uses ohicore::AlignDataYears() rather than ohicore::SelectLayersData()
+
+  scen_year <- layers$data$scenario_year
+
+
+  ao_stock_status <- AlignDataYears(layer_nm="ao_stock_status", layers_obj=layers) %>%
+    dplyr::mutate(dimension = as.character(dimension)) %>%
+    dplyr::filter(scenario_year == scen_year) %>%
+    dplyr::select(scenario_year, rgn_id, dimension, score)
+  ## status as NA indicates missing data, not that it is not applicable...
+
+  ao_stock_slope <- AlignDataYears(layer_nm="ao_stock_slope", layers_obj=layers) %>%
+    dplyr::mutate(dimension = as.character(dimension)) %>%
+    dplyr::filter(scenario_year == scen_year) %>%
+    dplyr::select(scenario_year, rgn_id, dimension, score)
+
+
+  ## the AO goal defined by the OHI framework has 3 components: stock status, access, need
+  ## currently the BHI only addresses the stock status
+
+  ## STOCK STATUS
+  ## status and trend slope are calculated in the ao_prep of the bhi-prep repo
+  ## calculated on the HOLAS basin scale and applied to the BHI regions
+
+  ## TREND
+  ## the trend slope is not yet modified by the future year of interest (5 years from now)
+
+  trend_n_years <- 5  # number of years for trend
+
+  ao_stock_trend <- ao_stock_slope %>%
+    dplyr::mutate(score = score * trend_n_years)
+
+
+  ## join status and trend
+  ao_stock <- dplyr::bind_rows(ao_stock_status, ao_stock_trend) %>%
+    dplyr::rename(region_id = rgn_id)
+
+
+
+  ## in future check whether data is available for other sub-componenets!
+  ## if have access or need data layers, incorporate following example of ohi-global:
+  # sustainability <- 1.0
+  # r <- AlignDataYears(layer_nm="ao_access", layers_obj=layers) %>%
+  #   dplyr::rename(region_id = rgn_id, access = value) %>%
+  #   na.omit()
+  #
+  # ry <- AlignDataYears(layer_nm="ao_need", layers_obj=layers) %>%
+  #   dplyr::left_join(r, by = c("region_id", "scenario_year")) %>%
+  #   dplyr::mutate(Du = (1 - need) * (1 - access)) %>%
+  #   dplyr::mutate(status = (1 - Du) * sustainability * 100)
+  #
+  # status <- ry %>%
+  #   dplyr::filter(scenario_year == scen_year) %>%
+  #   dplyr::select(region_id, score = status) %>%
+  #   dplyr::mutate(dimension = "status")
+  #
+  # trend_years <- (scen_year - 4):(scen_year)
+  # trend <- ohicore::CalculateTrend(status_data = ry, trend_years = trend_years)
+  #
+  # scores <- rbind(status, trend) %>%
+  #   dplyr::mutate(goal = "AO")
+
+
+  scores <- ao_stock %>%
+    dplyr::select(region_id, dimension, score) %>%
+    dplyr::mutate(goal = "AO") %>%
+    dplyr::arrange(goal, region_id)
+
+  return(scores)
+
+} ## End AO function
--- convert_to_multiyear_routine.R
+++ convert_to_multiyear_routine.R
@@ -19,6 +19,7 @@
 base_path <- "/Users/eleanorecampbell/Desktop/GitHub/" # set based on own local file organization...
 source(file.path(base_path, "bhi/R/common.R"))
 source(file.path(base_path, "bhi/R/checking.R"))
+source(file.path(base_path, "bhi/R/reconfiguring.R"))
 
 ### layers from archive
 archive_filepath <- file.path(base_path, "bhi-1.0-archive/baltic2015")
@@ -29,24 +30,24 @@ names(layers$data)
 bhi_layers <- read_csv(file.path(base_path, "bhi/baltic/layers.csv"))
 bhi_archive_layers <- read_csv(file.path(archive_filepath, "layers.csv"))
 
-### conf from current bhi repo
 setwd(file.path(base_path, "bhi/baltic"))
-conf <- ohicore::Conf("conf")
-current_conf_scen_data_yrs <- conf$scenario_data_years
-current_conf_scen_data_yrs$layer_name %>% unique()
+
 
 ############################################################################
 ######## REPEAT BELOW STEPS AS NECESSARY TO INCORPORATE ALL NECESSARY LAYERS
 
-names(layers$data) %>% grep(pattern = "fp_.*", value = TRUE) # can subset, eg here by 'fp' prefix
+consider_goal_prefix <- "ao"
 
+## these are the names of data layers from the layers object created from archive
+names(layers$data) %>%
+  grep(pattern = paste0(consider_goal_prefix, "_.*"),
+       value = TRUE)
 
 ## CHOOSE LAYER TO LOOK AT FROM ARCHIVED LAYERS
-consider_layer <- layers$data$fp_wildcaught_weight
+consider_layer <- layers$data$ao_stock_status # View(consider_layer)
 head(consider_layer) # View(consider_layer)
-consider_lyr_nm <- "fp_wildcaught_weight"
+consider_lyr_nm <- "ao_stock_status"
 
-consider_goal_prefix <- "fp"
 
 if("year" %in% names(consider_layer)){
   consider_layer$year %>% unique() %>% sort() # years we can have in scenario_data_years.csv as data years
@@ -71,39 +72,69 @@ fn <- str_match(bhi_archive_layers$filename, paste0(consider_lyr_nm, ".*.csv")) 
   lapply(function(x){x[!is.na(x)]}) %>%
   unlist()
 
+## for purposes of transitioning repo to multi-year framework before new data prep, add dummy year col
 layer_archive_version <- read_csv(file.path(archive_filepath, "layers", fn))
 head(layer_archive_version)
 yr_col_added <- layer_archive_version %>%
   mutate(year = 2014) # temporary for setting up bhi repo!
-## will review how years are added when doing actual data prep!!!
+## will review how years are actually included when doing actual data prep for new assessment!!!!!
+head(yr_col_added)
 
 ## save version with year column to the bhi repo
 write_csv(yr_col_added, file.path(base_path, "bhi/baltic/layers", fn))
-
 datayears_tab <- read_csv(file.path(base_path, "bhi/baltic/layers", fn))
-head(datayears_tab)
 
-## UPDATE SCENARIO_DATA_YEARS FOR SELECTED LAYER
-## if layer is not in scenario_data_years csv, need to go update scenario_data_years.csv in baltic/conf
-current_conf_scen_data_yrs %>%
-  filter(layer_name == consider_lyr_nm) # if 0 rows, need to add with scenario_data_include()
 
-View(current_conf_scen_data_yrs)
+## UPDATING SCENARIO_DATA_YEARS FOR SELECTED LAYER
 
-tmp <- datayears_tab %>%
+tmp <- datayears_tab %>% # group_by(dimension) %>%
   group_by(rgn_id) %>%
   summarise(n = n(),
             min_yr = min(year),
             max_yr = max(year)) # View(tmp)
-datayrs <- 2014
+datayrs <- 2014 # assign based on data range if it exists!
 
-update_scen_data_yrs <- scenario_data_include(current_conf_scen_data_yrs, # %>%
-                                                # filter(is.na(str_match(layer_name, "fp_"))),
+## if layer is not in scenario_data_years csv, need to go update scenario_data_years.csv in baltic/conf
+## each time a layer is transitioned recreate scenario_data_years by recreating conf
+conf <- ohicore::Conf("conf")
+current_conf_scen_data_yrs <- conf$scenario_data_years
+current_conf_scen_data_yrs$layer_name %>% unique()
+
+current_conf_scen_data_yrs %>%
+  filter(layer_name == consider_lyr_nm) # if 0 rows, add it to the table w scenario_data_include()
+View(current_conf_scen_data_yrs)
+
+rm_lyrs <- setdiff(
+  current_conf_scen_data_yrs$layer_name %>%
+    grep(pattern = paste0(consider_goal_prefix, "_.*"), value = TRUE) %>% unique(),
+  names(layers$data) %>%
+    grep(pattern = paste0(consider_goal_prefix, "_.*"), value = TRUE))
+## consider_goal_prefix defined above as whatever goal is being transitioned...
+rm_lyrs
+
+
+incl_new_lyrs <- setdiff(
+  names(layers$data) %>%
+    grep(pattern = paste0(consider_goal_prefix, "_.*"), value = TRUE),
+  current_conf_scen_data_yrs$layer_name %>%
+    grep(pattern = paste0(consider_goal_prefix, "_.*"), value = TRUE) %>% unique())
+
+if(length(rm_lyrs) != 0){
+  base_scen_data_yrs <- current_conf_scen_data_yrs %>%
+    filter(is.na(str_match(layer_name, paste0(consider_goal_prefix, "_"))))
+} else {
+  base_scen_data_yrs <- current_conf_scen_data_yrs
+}
+
+update_scen_data_yrs <- scenario_data_include(base_scen_data_yrs,
                                               2015:2018,
-                                              new_lyrs = consider_lyr_nm,
-                                              rename_lyrs = "") %>%
+                                              new_lyrs = incl_new_lyrs) %>%
   mutate(data_year = ifelse(is.na(data_year), 2014, data_year)) # assign data year as 2014 for now...
-write_csv(update_scen_data_yrs, "./conf/scenario_data_years.csv")
+View(update_scen_data_yrs)
+
+## can use scenario_data_align() IF
+## 1. have a set of data years already within prepped data eg with FIS or MAR goals OR
+## 2. already have the given layer within scenario_data_years.csv (or just do above using incl_new_lyrs obj...)
 
 # align_scenario_data_yrs <- scenario_data_align(current_conf_scen_data_yrs,
 #                                                "mar_sustainability_score",
@@ -112,9 +143,21 @@ write_csv(update_scen_data_yrs, "./conf/scenario_data_years.csv")
 # write_csv(align_scenario_data_yrs, "./conf/scenario_data_years.csv")
 
 
+update_scen_data_yrs %>%
+  filter(!is.na(str_match(layer_name,
+                          paste0(consider_goal_prefix, "_"))))
+
+write_csv(update_scen_data_yrs, "./conf/scenario_data_years.csv")
+
 ######## REPEAT ABOVE STEPS AS NECESSARY TO INCORPORATE ALL NECESSARY LAYERS
 ############################################################################
 ############################################################################
+
+## confirm all layers have been dealt with
+names(layers$data) %>%
+  grep(pattern = paste0(consider_goal_prefix, "_.*"),
+       value = TRUE)
+
 
 
 ## UPDATE LAYERS.CSV AS NEEDED
@@ -146,4 +189,9 @@ scorelist <- lapply(scenario_yrs, function(yr){
     dplyr::mutate(year = yr)}) %>% dplyr::bind_rows()
 
 v2015scores <- read_csv(file.path(archive_filepath, "scores.csv")) %>% mutate(year = 2015)
-compare_scores(scorelist, 2015, v2015scores, 2015)
+tmp <- compare_scores(scorelist, 2015, v2015scores, 2015)
+
+library(plotly)
+ggplotly(tmp[[1]])
+
+tmp[[2]]
