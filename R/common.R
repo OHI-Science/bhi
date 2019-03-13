4113cbbef7365006693b629c879cb7ca2704dba4
Eleanore Campbell <eleanorecampbell@eduroam-10-200-20-34.local.su.se>
2019-03-13 12:32:21 +0100

    still don't really know how I want to deal with directories...

--- R/common.R
+++ R/common.R
@@ -8,6 +8,7 @@ library(here)
 dir_bhi <- here::here()
 dir_sp <- file.path(dir_bhi, "spatial")
 dir_R <- file.path(dir_bhi, "R")
+dir_baltic <- file.path(dir_bhi, "baltic")
 
 dir_B <- c("Darwin" = "/Volumes/BHI_share", # "Windows" = ?
            "Linux" = "/home/shares/ohi")[[ Sys.info()[["sysname"]] ]]
@@ -17,3 +18,5 @@ if (Sys.info()[["sysname"]] != "Linux" & !file.exists(dir_B)){ # warning if BHI 
                  sprintf("Do you need to mount the BHI server: %s?", dir_B)))
 }
 dir_B <- file.path(dir_B, "BHI 2.0") # CHANGE MAIN AUX BHI DIRECTORY HERE!
+
+setwd(dir_baltic) # CHANGE DEFAULT WORKING DIRECTORY (WILL THIS BREAK STUFF?!)
