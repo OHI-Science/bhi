2dda696aeb8c6de60e51c748990262f740ac2e00
Eleanore Campbell <eleanorecampbell@eduroam-10-200-20-34.local.su.se>
2019-03-13 11:08:08 +0100

    add calculating.R- moved the single function from common.R here so common.R is only for loading libraries and directory paths

---
+++ R/calculating.R
@@ -0,0 +1,44 @@
+## Libraries
+source(file.path("R", "common.R"))
+library(ohicore)
+library(broom)
+
+## Functions
+
+#' calculate BHI scores for each scenario year and save to a single csv file
+#'
+#' @param assessment_path the bhi repository 'baltic' subfolder, wherever that is on your local computer
+#' @param scenario_yrs which years scores are to be calculated for
+#' @param scores_path the directory where to save the scores.csv, by default in the 'baltic' (assessment) folder
+#'
+#' @return
+
+calculate_scores <- function(assessment_path, scenario_yrs, scores_path = "."){
+
+  currentwd <- getwd()
+  setwd(assessment_path)
+
+  ## load scenario configuration
+  conf <- ohicore::Conf("conf")
+
+  ## check that scenario layers files in the \layers folder match layers.csv registration
+  ohicore::CheckLayers("layers.csv", "layers", flds_id=conf$config$layers_id_fields)
+
+  ## load scenario layers for ohicore to access
+  layers <- ohicore::Layers("layers.csv", "layers")
+
+  scorelist <- lapply(scenario_yrs, function(yr){
+
+    print(sprintf("For assessment year %s", yr))
+    layers$data$scenario_year <- yr
+    scores_scenario_year <- ohicore::CalculateAll(conf, layers) %>%
+      dplyr::mutate(year = yr)}) %>%
+    dplyr::bind_rows()
+
+  ## write csv by default to 'assessment_path'
+  readr::write_csv(scorelist,
+                   sprintf("%s/scores.csv", scores_path),
+                   na = "")
+
+  setwd(currentwd)
+}
