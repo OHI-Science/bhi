bc7d14c1db11072fc3f431ef4d0eb1099e3bfab8
Eleanore Campbell <eleanorecampbell@eduroam-10-200-20-34.local.su.se>
2019-03-13 10:45:55 +0100

    common.R, setting directories...

--- R/common.R
+++ R/common.R
@@ -1,13 +1,21 @@
 ## Libraries
-library(tidyverse)
-library(here)
+library(dplyr)
+library(readr)
 library(tools)
+library(here)
 library(broom)
 library(ohicore)
 
 ## Directories
 dir_bhi <- here::here()
 dir_R <- file.path(dir_bhi, "R")
+
+dir_B <- c("Darwin" = "/Volumes/BHI_share", # "Windows" = ?
+           "Linux" = "/home/shares/ohi")[[ Sys.info()[["sysname"]] ]]
+if (Sys.info()[["sysname"]] != "Linux" & !file.exists(dir_B)){ # warning if BHI internal directory doesn't exist
+  warning(sprintf("The BHI directory dir_B set in R/common.R does not exist. Do you need to mount the BHI server: %s?", dir_B))
+}
+dir_B <- file.path(dir_B, "BHI 2.0") # CHANGE MAIN AUX BHI DIRECTORY HERE!
 
 ## Functions
 
@@ -38,7 +46,8 @@ calculate_scores <- function(assessment_path, scenario_yrs, scores_path = "."){
     print(sprintf("For assessment year %s", yr))
     layers$data$scenario_year <- yr
     scores_scenario_year <- ohicore::CalculateAll(conf, layers) %>%
-      dplyr::mutate(year = yr)}) %>% dplyr::bind_rows()
+      dplyr::mutate(year = yr)}) %>%
+    dplyr::bind_rows()
 
   ## write csv by default to 'assessment_path'
   readr::write_csv(scorelist,
