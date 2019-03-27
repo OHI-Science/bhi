cc78ea7f9349926391282b8dd6d5af2e8d78bdf6
Eleanore Campbell <eleanorecampbell@eduroam-10-200-20-34.local.su.se>
2019-03-27 15:28:40 +0100

    rename update_alt_layers_tab to update_alt_layers and fix some small issues; move configure_functions and configure_layers from testing.R which will be deleted; rename layers_edit to layerscsv_edit; copy_lyrs_from_bhiprep function added in reconfiguring.R

--- R/reconfiguring.R
+++ R/reconfiguring.R
@@ -3,6 +3,7 @@ source(file.path(here::here(), "R", "common.R"))
 library(tools)
 library(tidyr)
 library(tibble)
+library(httr)
 
 ## Functions
 
@@ -144,27 +145,38 @@ scenario_data_align <- function(scen_data_years, lyr_name, data_yrs, scen_yrs, a
 #'
 #' @return
 
-update_alt_layers_tab <- function(assessment_path, prep_path, assess_year){
+update_alt_layers <- function(assessment_path = dir_baltic, prep_path = dir_prep, assess_year){
 
-  # alt_layers <- readr::read_delim(sprintf("%s/testing/alt_layers_full_table.csv", assessment_path), delim = ";")
+  print("if creating an entirely new layer not just a different version, you must have added it in layers.csv")
+  lyrscsv <- readr::read_csv(file.path(assessment_path, "layers.csv"))
 
-  alt_layers <- readr::read_csv(sprintf("%s/testing/%s", assessment_path, "alt_layers_full_table.csv"))
-  layer_names <- alt_layers$layer %>% unique() %>% as.list()
+  alt_layers <- readr::read_csv(file.path(assessment_path, "testing", "alt_layers.csv"))
+  if(dim(alt_layers)[2] == 1 & stringr::str_detect(names(alt_layers), ";")){
+    alt_layers <- readr::read_delim(file.path(assessment_path, "testing", "alt_layers.csv"), delim = ";")
+  }
+  if(dim(alt_layers)[2] == 1){
+    stop(print("double check that alt_layers.csv is delimited with commas or semicolons"))
+  }
+  layer_names <- lyrscsv$layer %>% unique() %>% as.list()
 
-  ## identify new layers to add to the 'alt_layers_full_table.csv' table
+  ## identify new layers (in the bhi-prep repo) to add to the full 'alt_layers.csv' table
   add_alt_layers <- list.files(prep_path, recursive = TRUE) %>%
-    grep(pattern = sprintf(".*v%s/output/[a-z0-9_]+.csv$", assess_year), value = TRUE) %>%
-    enframe(name = NULL) %>%
+    grep(pattern = file.path(sprintf(".*v%s", assess_year), "output", "[a-z0-9_]+.csv$"),
+         value = TRUE) %>% # gets things of form v2019/output/goal_layer_final.csv
+    stringr::str_remove("prep") %>%
+    enframe(name = NULL) %>% # makes into a tibble
     dplyr::mutate(filename = basename(as.character(value)),
                   goal = stringr::str_extract(value, pattern = "[a-zA-Z]+"),
                   use = NA,
                   layer_names = list(layer_names),
-                  subfolder = stringr::str_extract(value, pattern = "(\\w+ ?){1}/v2") %>%
+                  subfolder = stringr::str_extract(value, pattern = "(\\w+ ?){1}/v2") %>% # ...but beyond year 2999?
                     stringr::str_remove("/v2")) %>%
 
     dplyr::rowwise() %>%
-    mutate(layer = stringr::str_match(filename, as.character(layer_names)) %>%
-             lapply(function(x){x[!is.na(x)]}) %>%
+    dplyr::mutate(match_found = list(stringr::str_match(filename, as.character(layer_names) %>%
+                                                          stringr::str_remove_all("\"")))) %>%
+    dplyr::mutate(layer = match_found %>%
+             lapply(function(x){ x[!is.na(x)] }) %>%
              unlist(),
            subfolder = ifelse(subfolder == goal, NA, subfolder)) %>%
     ungroup() %>%
@@ -193,7 +205,7 @@ update_alt_layers_tab <- function(assessment_path, prep_path, assess_year){
 #'
 #' @return
 
-layers_edit <- function(layers_object, lyr_file, lyr_name, assessment_path, update_with = NULL, write = FALSE){
+layerscsv_edit <- function(layers_object, lyr_file, lyr_name, assessment_path, update_with = NULL, write = FALSE){
 
   lyr_file_data <- readr::read_csv(lyr_file)
   lyr_file_name <- basename(lyr_file)
@@ -255,5 +267,175 @@ layers_edit <- function(layers_object, lyr_file, lyr_name, assessment_path, upda
     print("IMPORTANT: now you must update scenario_data_years.csv!")
 
     return(list(updated_tab, lyr_file_data))
+  }
+}
+
+
+#' configure functions.R for testing
+#'
+#' compiles files listed in a local 'test_functions.csv' into the single \code{functions.R} that ohicore needs
+#'
+#' @param assessment_path filepath specifying the assessment main folder containing 'conf' and 'testing' folders
+#' @param test_funs_list a list of filepaths from (not including) 'testing/alt_functions' to the functions we want to use in analysis
+#' @return revises functions.R at existing location in 'conf' folder; no direct output
+
+configure_functions <- function(assessment_path, test_funs_list = NULL){
+
+  functionsR_path <- file.path(assessment_path, "conf", "functions.R")
+  fun_scripts_path <- file.path(assessment_path, "testing", "alt_functions")
+
+  ## get filepaths list of functions we want to collect into functions.R
+  if(is.null(test_funs_list)){ # only works if 'test_functions.csv' exists in directory
+    if(file.exists("test_functions.csv")){
+      fun_scripts_lst <- readr::read_csv("test_functions.csv", col_names = FALSE) %>%
+        as.list() %>%
+        lapply(function(x) file.path(assessment_path, "testing", "alt_functions", x))
+    } else { stop("no 'test functions' list provided, and no 'test_function.csv' found") }
+  } else { # better to actually provide the list
+    fun_scripts_lst <- test_funs_list %>%
+      lapply(function(x) file.path(assessment_path, "testing", "alt_functions", x))
+  }
+
+  ## confirm all specified function versions actually exist before overwriting functions.R
+  chk <- file.exists(fun_scripts_lst[[1]])
+  if(any(!chk)){
+    m <- basename(fun_scripts_lst[[1]][!chk]) # file.exists is FALSE ie, functions aren't at given location
+    stop(sprintf("the following do not exist in given locations: %s",
+                 paste(m, collapse = ", ")))
+  }
+
+  sink(file = functionsR_path, append = FALSE) # overwrite functions.R with first function
+  cat(scan(file = fun_scripts_lst[[1]][1], what = "character", sep = "\n"), sep = "\n")
+
+  sink(file = functionsR_path, append = TRUE) # append subsequent functions
+  for(i in fun_scripts_lst[[1]][-1]){
+    cat(scan(file = i, what = "character", sep = "\n"), sep = "\n")
+  }
+  closeAllConnections() # stop the sinking
+}
+
+
+#' configure layers for testing
+#'
+#' copies the versions of layers we want to use for testing (as specified in the 'testing/alt_layers.csv') into `bhi-prep/prep/layers`
+#' the `copy_layers_for_assessment` function in `bhi/R/common.R` is used afterwards to copy layers to the layers folder in the assessment repo (bhi/baltic)
+#' compares the specified layers to the layers named in \code{functions.R} to confirm all layers needed given the models, will be copied to 'layers'
+#' automatically writes the name of copied file into 'filename' column of 'layers.csv'
+#'
+#' @param assessment_path filepath specifying the assessment main folder containing 'conf' and 'testing' folders
+#' @param prep_path bhi-prep filepath specifying the main folder containing goal/pressure/resilience subfolders
+#' @param test_path filepath to subfolder of 'testing' that contains everything fro a sigle analysis; any outputs are saved to/read from here
+#'
+#' @return a table comparing layers required by \code{functions.R} to those specified in alt_layers_full_table.csv; revises 'layers.csv' and contents of 'layers' folder
+
+configure_layers <- function(assessment_path, prep_path, test_path){
+
+  test_layers_path <- file.path(assessment_path, "testing", "alt_layers_full_table.csv")
+  functionsR_path <- file.path(assessment_path, "conf", "functions.R")
+
+  print("this function assumes you've already made sure alt_layers_full_table.csv is up to date!")
+  use_layers <- readr::read_delim(test_layers_path, delim = ";") %>%
+    dplyr::filter(use == "YES")
+
+  ## extract names of layers specified in functions.R (i.e. which do functions.R require)
+  functionsR_layers <- scan(file = functionsR_path, what = "character", sep = "\n") %>%
+    grep(pattern = "\\s*#{1,}.*", value = TRUE, invert = TRUE) %>% # remove commented lines
+    gsub(pattern = "layer_nm\\s{1,}=\\s{1,}", replacement = "layer_nm=") %>%
+    gsub(pattern = "", replacement = "layer_nm=") %>% # to catch pressure + resilience layers given with a different pattern
+    stringr::str_split(" ") %>%
+    unlist() %>%
+    stringr::str_subset("layer_nm.*") %>% # pattern to ID layer fed into a function within fuctions.R
+    stringr::str_extract("\"[a-z0-9_]*\"") %>%
+    stringr::str_sort()
+
+  ## checking: compare layers required by functions.R to the layers specified in table
+  check_layers_table <- tibble::tibble(layer = functionsR_layers, fun_layer = "YES") %>%
+    dplyr::full_join(use_layers, by = "layer") %>%
+    dplyr::mutate(required = ifelse(is.na(fun_layer) == TRUE, "not required", NA)) %>%
+    dplyr::mutate(specified = ifelse(is.na(filename) == TRUE, "specify version", NA)) %>%
+    dplyr::group_by(layer) %>%
+    dplyr::add_tally() %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(multiple = ifelse(n > 1, "overspecified", "")) %>%
+    dplyr::select(-fun_layer, -use, -n)
+  readr::write_csv(check_layers_table,
+                   file.path(test_path, "test_layers_used.csv"),
+                   append = FALSE)
+
+  one_to_one <- ifelse(length(unique(check_layers_table$multiple)) > 1, FALSE, TRUE)
+  onto <- ifelse(length(unique(check_layers_table$specified)) > 1, FALSE, TRUE)
+  stopifnot(one_to_one, onto) # stop if error- don't proceed to layer-copying step
+
+  ## copy over the layer versions for calculations
+  use_layers <- check_layers_table %>%
+    dplyr::filter(required != "not required") %>%
+    dplyr::mutate(path = ifelse(is.na(subfolder) == FALSE,
+                                paste(goal, subfolder, layer, filename, sep = "/"),
+                                paste(goal, layer, filename, sep = "/")))
+
+  continue <- readline(prompt = sprintf("overwrite current contents of '%s/layers' (continue: yes/no)? ", prep_path))
+  stopifnot(continue == "yes")
+
+  ## copy layers to 'layers' folder, and write version into 'filename' column of 'layers.csv'
+  for(i in 1:nrow(use_layers)){
+
+    use_layer_path <- sprintf("%s/%s", prep_path, use_layers$paths[i]) # the layer file to copy is located here
+    put_layer_path <- sprintf("%s/layers/%s", prep_path, use_layers$filename[i]) # where to put copied layer
+    file.copy(use_layer_path, put_layer_path, overwrite = TRUE)
+
+    ## regex notes: layer must be left of filename col, and filename is assumed to be 5-50 alphanumeric char
+    tmp <- scan(sprintf("%s/layers.csv", assessment_path), what = "character", sep = "\n") %>%
+      stringr::str_replace(pattern = sprintf("%s\\\",\\\"\\S{5,50}.csv", use_layers$layer[i]),
+                           sprintf("%s\\\",\\\"%s", use_layers$layer[i], use_layers$filename[i])) %>%
+      stringr::str_split(",(?!\\s+)", simplify = TRUE) %>%
+      as.data.frame()
+    colnames(tmp) <- c(scan(sprintf("%s/layers.csv", assessment_path), what = "character", sep = ",")[1:26])
+    write.csv(tmp[-1,], sprintf("%s/layers.csv", assessment_path), row.names = FALSE)
+
+    print(sprintf("copied %s to layers folder", use_layers$filename[i]))
+  }
+
+  return(check_layers_table) # return so can visually inspect
+}
+#
+#
+# configure_pressures
+#
+#
+# configure_resilience <- function(){
+#
+# }
+
+
+#' copy prepared layers from bhi-prep to bhi repo
+#'
+#' copies layers from prep 'bhi-prep/prep/layers' to the assessment 'bhi/baltic/layers' folder, from github repo url
+#'
+#' @param assessment_path file path to assessment folder within bhi repo
+#' @param copy_layers
+#' @param repo_loc url pointing to the bhi repo on github
+#'
+#' @return
+
+copy_lyrs_from_bhiprep <- function(assessment_path, copy_layers = list("all"), repo_loc = NULL){
+
+  if(is.null(repo_loc)){
+    repo_loc <- paste0(prep_repo_raw, "/master") # raw bhi-prep github repo url, prep_repo_raw from common.R
+  }
+  if(unlist(copy_layers) == "all"){
+    print("copying over all layers from bhi-prep repository 'layers' folder")
+    print("note: will overwrite layers in bhi repo 'layers' folder with bhi-prep versions from github, of same name")
+    filelist <- bhiprep_github_layers(github_api_url = bhi_prep_api) # function defined and bhi_prep_api assigned in common.R
+  } else {
+    filelist <- data.frame(V1 = unlist(copy_layers))
+  }
+  for(i in 1:length(filelist$V1)){
+    tmp <- filelist[i,1] %>% as.character()
+    if(stringr::str_length(tmp) != 0){
+      url <- sprintf("%s/%s", repo_loc, tmp)
+      name_of_layer <- tmp %>% stringr::str_extract("[a-z0-9_]+.csv$")
+      httr::GET(url, write_disk(sprintf("%s/layers/%s", assessment_path, name_of_layer),
+                                overwrite = TRUE))
+    }
   }
 }
