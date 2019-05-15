e84523634d9bac91137e1f1afeec7409827e570f
Eleanore Campbell <eleanorecampbell@eduroam-10-200-59-97.local.su.se>
2019-05-15 15:32:43 +0200

    sp goal added

---
+++ baltic/testing/alt_functions/multiyear_v2015/sp_multiyear_v2015.R
@@ -0,0 +1,25 @@
+
+SP <- function(layers, scores){
+
+  scen_year <- layers$data$scenario_year
+
+  ## From code in 'functions.R SP' of v2015 BHI assessment, see bhi-1.0-archive github repo
+  ## Revised to use multi-year framework, incorporating scenario_data_years
+  ## Uses ohicore::AlignDataYears() rather than ohicore::SelectLayersData()
+
+  ## to calculate the four SP dimesions, average those dimensions for ICO and LSP
+
+  s <- scores %>%
+    filter(goal %in% c("ICO", "LSP"),
+           dimension %in% c("status", "trend", "future", "score")) %>%
+    group_by(region_id, dimension) %>%
+    summarize(score = mean(score, na.rm = TRUE)) %>%
+    ungroup() %>%
+    arrange(region_id) %>%
+    mutate(goal = "SP") %>%
+    dplyr::select(region_id, goal, dimension, score) %>%
+    data.frame()
+
+  return(rbind(scores, s))
+
+} ## End SP function
