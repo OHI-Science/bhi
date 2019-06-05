6657a4aec9025b6ff9887affd2615d441e5ed1ba
Eleanore Campbell <eleanorecampbell@eduroam-10-200-50-188.local.su.se>
2019-06-05 15:12:33 +0200

    reorganizing config.R, pressure_elements and resilience_elements as tailored to bhi

--- baltic/conf/config.R
+++ baltic/conf/config.R
@@ -1,48 +1,73 @@
-# region data layers----
-# a list of possible id fields used in datalayers (most will use rgn_id, but not always)
-layers_id_fields    = c('rgn_id','cntry_key', 'fao_id', 'fao_saup_id', 'country_id','saup_id','fao_ohi_id') 
 
-# the official list of regions (and corresponding names)
-# (note: this is a .csv file in the layers folder)
-layer_region_labels = 'rgn_global'   
+## region data layers and spatial config ----
+## a list of possible id fields used in datalayers (most will use rgn_id, but not always)
+layers_id_fields <- c("rgn_id", "cntry_key", "fao_id", "fao_saup_id", "country_id", "saup_id", "fao_ohi_id")
 
-# the official ocean areas of each region (used to weight each subregions contribution to the region score)
-# (note: this is a .csv file in the layers folder)
-layer_region_areas  = 'rgn_area'
+## the official list of regions, and corresponding names
+## note: this is a csv file in the layers folder
+layer_region_labels <- "rgn_labels"
 
-# pressures & resilience matrices ----
+## the official ocean areas of each region, used to weight each subregions contribution to the region score
+## note: this is a csv file in the layers folder
+layer_region_areas <- "rgn_area"
 
-# For goals with elements (e.g., for coastal protection: mangrove, saltmarsh, seagrass), these data layers describe how to weight the contribution of each goal element to calculate the final goal pressure and resilience dimensions.
-# (note: these are .csv files in the layers folder)
-resilience_element = list('NP'  = 'np_harvest_product_weight',
-                          'CS'  = 'element_wts_cs_km2_x_storage',      # populated in CS() in functions.R
-                          'CP'  = 'element_wts_cp_km2_x_protection',   # populated in CP() in functions.R
-                          'HAB' = 'element_wts_hab_pres_abs')          # populated in HAB() in functions.R
-
-pressures_element  = list('NP'  = 'np_harvest_product_weight',
-                          'CS'  = 'element_wts_cs_km2_x_storage',      # populated in CS() in functions.R
-                          'CP'  = 'element_wts_cp_km2_x_protection',   # populated in CS() in functions.R
-                          'LIV' = 'le_sector_weight',
-                          'ECO' = 'le_sector_weight',
-                          'HAB' = 'element_wts_hab_pres_abs')          # populated in CS() in functions.R
+## spatial configuration, to be used by shiny app and future ohicore mapping functions that use leaflet
+geojson <- "spatial/regions_gcs_simple2x.geojson"
 
 
-# constants
-# These are used in the Calculate functions in ohicore to calculate pressures/resilience/trend/likely future state
-pressures_gamma = 0.5  # The relative importance of social vs. ecological pressures (pressure = gamma * ecological + (1-gamma) * social)
-resilience_gamma = 0.5 # The relative importance of social vs. ecological resiliences (resilience = gamma * ecological + (1-gamma) * social)
-goal_discount = 1.0    # Used to calculate likely future state
-goal_beta = 0.67       # The relative importance of trend vs. pressure/resilience on likely future state; if goal_beta = 0.67, trend is twice as important as pressure/resilience.
+## pressures & resilience matrices ----
+
+## for goals with elements
+## these describe how to weight contribution of each element in calculation of pressure and resilience dimensions
+resilience_element <- NULL # for BHI no goals have elements currently
+pressures_element <- NULL # for BHI no goals have elements currently
+
+# resilience_element = list("NP"  = "np_harvest_product_weight",
+#                           "CS"  = "element_wts_cs_km2_x_storage",      # populated in CS() in functions.R
+#                           "CP"  = "element_wts_cp_km2_x_protection",   # populated in CP() in functions.R
+#                           "HAB" = "element_wts_hab_pres_abs")          # populated in HAB() in functions.R
+#
+# pressures_element  = list('NP'  = 'np_harvest_product_weight',
+#                           'CS'  = 'element_wts_cs_km2_x_storage',      # populated in CS() in functions.R
+#                           'CP'  = 'element_wts_cp_km2_x_protection',   # populated in CS() in functions.R
+#                           'LIV' = 'le_sector_weight',
+#                           'ECO' = 'le_sector_weight',
+#                           'HAB' = 'element_wts_hab_pres_abs')          # populated in CS() in functions.R
+
+
+## constants for dimensions' relative importance ----
+## used by ohicore::Calculate functions to calculate pressures/resilience/trend/likely future state
+
+## relative importance of social vs ecological pressures
+## (pressure = gamma * ecological + (1-gamma) * social)
+pressures_gamma = 0.5
+
+## relative importance of social vs ecological resiliences
+## (resilience = gamma * ecological + (1-gamma) * social)
+resilience_gamma = 0.5
+
+## used to calculate likely future state
+goal_discount = 1.0
+
+## relative importance of trend vs. pressure/resilience on likely future state
+## if goal_beta = 0.67, trend is twice as important as pressure/resilience
+goal_beta = 0.67
 default_trend = 0
 
-# spatial configuration (used by shiny app and future ohicore mapping functions that use leaflet)
-geojson = '../eez2015/spatial/rgn_offshore_gcs_mapshaper-simplify_x2_eez-only.geojson'
 
-# extra descriptions not covered by goals.description or layers.description, used in ohigui
-index_description = 'The overall Index represents the weighted average of all goal scores.'
-dimension_descriptions = c('score' = 'This dimension is an average of the current status and likely future.',
-                           'status' = 'This dimension represents the current value of a goal or sub-goal relative to its reference point.',
-                           'future' = 'For this dimension, the likely future is calculated as the projected status in 5 years, informed by the current status, continued trend, inflected upwards by resilience and downwards by pressures.',
-                           'trend' = 'This dimension represents the recent change in the value of the status. Unlike all other dimensions which range in value from 0 to 100, the trend ranges from -1 to 1, representing the steepest declines to increases respectively.',
-                           'pressures' = 'This dimension represents the anthropogenic stressors that negatively affect the ability of a goal to be delivered to people. Pressures can affect either ecological or social (i.e. human) systems.',
-                           'resilience' = 'This dimension represents the social, institutional, and ecological factors that positively affect the ability of a goal to be delivered to people.')
+## descriptions and metadata ----
+
+## extra descriptions not covered by goals.description or layers.description
+index_description <- "The overall Index represents the weighted average of all goal scores."
+
+loc <- file.path(dir_bhi, "supplement", "tables")
+dims_tab <- read_csv(file.path(loc, "dimension_descriptions.csv"))
+
+dimension_descriptions <- c(
+  "score" = dims_tab$description[1],
+  "status" = dims_tab$description[2],
+  "future" = dims_tab$description[3],
+  "trend" = dims_tab$description[4],
+  "pressures" = dims_tab$description[5],
+  "resilience" = dims_tab$description[6]
+  )
