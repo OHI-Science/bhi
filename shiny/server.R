348d34ee7aaccab443b579d1c2d827398602f8a2
Eleanore Campbell <eleanorecampbell@Eleanortes-MacBook-Pro.local>
2019-07-25 14:02:50 +0200

    initial commit, server.R

--- shiny/server.R
+++ shiny/server.R
@@ -1,3 +1,7 @@
-library(shiny)
-library(dplyr)
-library(pool)
+function(input, output, session){
+
+  ## Overall Index
+  output$flowerplot <- renderUI({
+    tags$img(src = "https://raw.github.com/OHI-Northeast/ne-scores/master/region/reports/figures/flower_USNortheast.png", height = 450, width = 600)
+    })
+}
