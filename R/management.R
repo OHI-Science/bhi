8ecbc86a2d2b393336406bff1031a0064151c0ff
Eleanore Campbell <eleanorecampbell@Eleanortes-MBP.lan>
2019-08-01 21:38:26 +0200

    moving info to supplement database doc

--- R/management.R
+++ R/management.R
@@ -1,39 +1,13 @@
 ## Libraries
 source(file.path(here::here(), "R", "common.R"))
-library(sf) # install.packages("sf")
+library(sf)
 library(rgdal)
-library(raster)
-library(fasterize)
 library(tidytext)
 library(dbplyr)
 library(DBI)
 library(odbc)
 library(config)
 library(httr)
-
-## helpful database functions in dplyr: copy_to,
-## functions in DBI: dbConnect,
-## from dbplot: creates ggplot object using data within database
-
-
-## Extra Info
-
-## https://db.rstudio.com/best-practices/drivers/
-## https://db.rstudio.com/databases/postgresql/
-## https://db.rstudio.com/odbc/
-## Connecting to database w/ method supporting DBI package makes using dplyr as a front-end possible
-## odbc connects via open database connectivity protocol
-
-## Setting up OBCD drivers (MacOS instructions):
-## first install homebrew if not installed (https://brew.sh/)
-## then via terminal, install the unixODBC library and PostgreSQL ODBC ODBC Drivers:
-# brew install unixodbc
-# brew install psqlodbc
-
-## edit odbcinst.ini (driver options) and odbc.ini (connection options) files
-## to find exact location use: odbcinst -j
-## or rather than depending on DSNs use config package:
-## 'allows connection code in R to reference external file that defines values based on the environment'
 
 
 ## Functions
