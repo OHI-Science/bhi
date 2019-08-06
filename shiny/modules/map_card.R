b67fb2676d6fc9c250be367b211674b9e8daaa7b
Eleanore Campbell <eleanorecampbell@eduroam-10-200-61-237.local.su.se>
2019-08-06 15:44:41 +0200

    incorporating spatial unit and dimensions reactives in map_card

--- shiny/modules/map_card.R
+++ shiny/modules/map_card.R
@@ -6,25 +6,23 @@
 #' from https://github.com/OHI-Northeast/ne-dashboard/blob/master/modules/map_card.R
 
 ## map card ui function ----
+mapCardUI <- function(id,
+                      title_text = NULL,
+                      sub_title_text = NULL,
+                      source_text = NULL,
+                      select_type = c(NULL, "radio", "drop_down", "checkboxes"),
+                      select_choices = c(""),
+                      select_label = NULL,
+                      selected = NULL){
 
-map_ui <- function(id,
-                   title_text = NULL,
-                   sub_title_text = NULL,
-                   select_type = c(NULL, "radio", "drop_down", "checkboxes"),
-                   select_location = c(NULL, "above", "below"),
-                   select_choices = c(""),
-                   select_label = NULL,
-                   selected = NULL,
-                   source_text = NULL){
 
-  ## make namespace for the id-specific object
   ns <- NS(id)
 
-  ## selecting layout
-  if(missing(select_type) == TRUE){ # output without selection
+  ## selection layout
+  if(missing(select_type) == TRUE){ # without selection
     items <- leafletOutput(ns("plot"), height = 480)
-  } else {
 
+  } else {  # with selection
     if(select_type == "radio"){ # selection type
       select <- radioButtons(ns("select"),
                              choices = select_choices,
@@ -42,14 +40,7 @@ map_ui <- function(id,
                                    label = p(select_label),
                                    selected = selected)
     }
-
-    ## chart layout
-    if(select_location == "above"){
-      items <- list(select, leafletOutput(ns("plot"), height = 480))
-
-    } else if(select_location == "below"){
-      items <- list(leafletOutput(ns("plot"), height = 480), select)
-    }
+    items <- list(select, leafletOutput(ns("plot"), height = 480))
   }
 
   ## put together in box and return box
@@ -61,62 +52,45 @@ map_ui <- function(id,
 
 
 ## map card server function ----
+mapCard <- function(input,
+                    output,
+                    session,
+                    goal_code,
+                    dimension_selected,
+                    spatial_unit_selected,
+                    legend_title = NA,
+                    popup_title = NA,
+                    popup_add_field = NA,
+                    popup_add_field_title = NA){
 
-card_map <- function(input,
-                     output,
-                     session,
-                     data,
-                     field,
-                     goal_code,
-                     filter_field = NULL,
-                     display_field = NULL,
-                     legend_title = NA,
-                     popup_title = NA,
-                     popup_add_field = NA,
-                     popup_add_field_title = NA){
-
-  # shp <- sf::st_read("/Volumes/BHI_share/Shapefiles/BHI_shapefile", "BHI_shapefile") %>%
-  #   dplyr::mutate(Subbasin = as.character(Subbasin)) %>%
-  #   dplyr::mutate(Subbasin = ifelse(Subbasin == "Bothian Sea", "Bothnian Sea", Subbasin)) # NEED TO FIX THIS TYPO!!!!!!!!
-  shp <- sf::st_read("/Volumes/BHI_share/Shapefiles/HELCOM_subbasins_holasbasins", "HELCOM_subbasins_holasbasins")
-  scores_csv <- tbl(bhi_db_con, "scores2015") %>%
-    filter(dimension == "score") %>%
-    collect()
-
-  result <- leaflet_map(goal_code, basins_or_rgns = "subbasins", mapping_data_sp = NULL, # basin_or_rgns = "regions"
-                        shp = shp, scores_csv, simplify_level = 1,  dim = "score",
-                        year = 2014, include_legend = TRUE)
-
+  ## render and return the leaflet map etc
   output$plot <- renderLeaflet({
-    popup_text <- paste("<h5><strong>", popup_title, "</strong>", result$data_sf[[field]], "</h5>",
-                        "<h5><strong>", popup_add_field_title, "</strong>", result$data_sf[[popup_add_field]], "</h5>", sep = " ") # get popup
 
-    result$map # call map!
+    ## scores data from bhi database
+    scores_csv <- tbl(bhi_db_con, "scores2015") %>%
+      filter(dimension == d) %>%
+      collect()
+
+    ## for now, read in shp data...
+    if(spatial_unit_selected() == "subbasins"){
+      shp <- sf::st_read("/Volumes/BHI_share/Shapefiles/HELCOM_subbasins_holasbasins",
+                         "HELCOM_subbasins_holasbasins")
+    } else {
+      shp <- sf::st_read("/Volumes/BHI_share/Shapefiles/BHI_shapefile",
+                         "BHI_shapefile") %>%
+        dplyr::mutate(Subbasin = as.character(Subbasin)) %>%
+        dplyr::mutate(Subbasin = ifelse(Subbasin == "Bothian Sea",
+                                        "Bothnian Sea", Subbasin)) # NEED TO FIX THIS TYPO!!!!!!!!
+    }
+
+    result <- leaflet_map(goal_code, spatial_unit_selected(), mapping_data_sp = NULL,
+                          shp = shp, scores_csv, simplify_level = 1, dim = d, year = 2014,
+                          include_legend = TRUE, legend_title)
+
+    popup_text <- paste("<h5><strong>", popup_title, "</strong>",
+                        result$data_sf[[goal_code]], "</h5>",
+                        "<h5><strong>", popup_add_field_title, "</strong>",
+                        result$data_sf[[popup_add_field]], "</h5>", sep = " ")
+    result$map
   })
-
-
-  if(field != "input"){
-
-    output$plot <- renderLeaflet({
-      popup_text <- paste("<h5><strong>", popup_title, "</strong>", result$data_sf[[field]], "</h5>",
-                          "<h5><strong>", popup_add_field_title, "</strong>", result$data_sf[[popup_add_field]], "</h5>", sep = " ")
-
-      result$map # call map!
-    })
-
-  } else {
-    filter_field <- enquo(filter_field)
-    selected_data <- reactive({
-      df <- result$data_sf %>% filter(!!filter_field == input$select)
-      return(df)
-    })
-
-    output$plot <- renderLeaflet({
-      popup_text <- paste("<h5><strong>", popup_title, "</strong>", selected_data()[[display_field]], "</h5>",
-                          "<h5><strong>", popup_add_field_title, "</strong>", selected_data()[[popup_add_field]], "</h5>", sep = " ")
-
-      result$map # call map!
-    })
-
-  }
 }
