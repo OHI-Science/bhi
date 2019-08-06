3e4646d7de90ffb51286758a97f5a032b6048c16
Eleanore Campbell <eleanorecampbell@eduroam-10-200-61-237.local.su.se>
2019-08-06 15:42:18 +0200

    dimension and spatial unit (rgn vs basin) reactives

--- shiny/server.R
+++ shiny/server.R
@@ -1,39 +1,34 @@
 function(input, output, session){
 
+  spatial_unit <- reactive({input$spatial_unit})
+  dimension <- reactive({input$dimension})
+
   ## WELCOME ----
 
-  # # flowerplot
-  # output$flowerplot <- renderImage({
-  #   plot_obj <- make_flower_plot(
-  #     tbl(bhi_db_con, "scores2015") %>%
-  #       filter(dimension == "score") %>%
-  #       collect(),
-  #     rgn_id = 0, plot_year = 2014,
-  #     include_ranges = TRUE, labels = "arc") %>%
-  #
-  #     image_trim() %>%
-  #     image_scale("x350") %>%
-  #     image_border("white", "20x20")
-  #
-  #   tmpfile <- plot_obj %>%
-  #     image_write(tempfile(fileext = "jpg"), format = "jpg")
-  #   list(src = tmpfile, contentType = "image/jpeg")
-  # })
-  # overall index scores map
-  callModule(card_flower, "overall_baltic_flowerplot",
-             data = overall_baltic_flowerplot,
-             field = "score",
+  ## flowerplot
+  callModule(flowerplotCard, "baltic_flowerplot",
+             dimension = "score",
              region_id = 0)
 
-  # overall index scores map
-  callModule(card_map, "overall_baltic_map",
-             data = overall_baltic_map,
-             field = "scores",
+  ## overall index scores map
+  callModule(mapCard, "index_map",
              goal_code = "Index",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
              legend_title = "Regions Scores",
              popup_title = "Score:",
              popup_add_field = "Name",
              popup_add_field_title = "Basin:")
+
+
+  # callModule(map_barplot_card, "index_map",
+  #            field = "scores",
+  #            goal_code = "Index",
+  #            spatial_unit = basins_or_rgns,
+  #            legend_title = "Regions Scores",
+  #            popup_title = "Score:",
+  #            popup_add_field = "Name",
+  #            popup_add_field_title = "Basin:")
 
 
   ## AO ----
@@ -41,14 +36,14 @@ function(input, output, session){
 
 
 
-  callModule(card_map, "baltic_map_ao",
-             data = baltic_map_ao,
-             field = "scores",
-             goal_code = "AO",
-             legend_title = "Regions Scores",
-             popup_title = "Score:",
-             popup_add_field = "Name",
-             popup_add_field_title = "Basin:")
+  # callModule(card_map, "baltic_map_ao",
+  #            data = baltic_map_ao,
+  #            field = "scores",
+  #            goal_code = "AO",
+  #            legend_title = "Regions Scores",
+  #            popup_title = "Score:",
+  #            popup_add_field = "Name",
+  #            popup_add_field_title = "Basin:")
 
 
   ## BD ----
