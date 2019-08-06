65af9806cb01c1fef76c340450a757e075835924
Eleanore Campbell <eleanorecampbell@eduroam-10-200-61-237.local.su.se>
2019-08-06 15:40:45 +0200

    sourcing module functions in shiny global

--- shiny/global.R
+++ shiny/global.R
@@ -9,7 +9,13 @@ library(shinyWidgets)
 library(pool)
 library(highcharter)
 
+
 ## Functions
+
+source(file.path(here::here(), "shiny", "modules", "map_card.R"))
+source(file.path(here::here(), "shiny", "modules", "map_barplot_card.R"))
+source(file.path(here::here(), "shiny", "modules", "barplot_card.R"))
+source(file.path(here::here(), "shiny", "modules", "flowerplot_card.R"))
 
 #' ui function to expand and contract subitems in menu sidebar
 #' from convertMenuItem by Jamie Afflerbach https://github.com/OHI-Northeast/ne-dashboard/tree/master/functions
