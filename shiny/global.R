ba34433d069888f0a3c4f29d2cdcdc03ea38c806
Eleanore Campbell <eleanorecampbell@eduroam-10-200-61-237.local.su.se>
2019-08-19 15:05:30 +0200

    add some common urls, helper functions

--- shiny/global.R
+++ shiny/global.R
@@ -1,4 +1,4 @@
-## Libraries ----
+## Libraries Etc ----
 source(file.path(here::here(), "R", "visualization.R"))
 source(file.path(here::here(), "R", "flowerplot.R"))
 source(file.path(here::here(), "R", "maps.R"))
@@ -6,10 +6,13 @@ library(shiny)
 library(shinydashboard)
 library(shinyjs)
 library(shinyWidgets)
+library(shinycssloaders)
 library(pool)
 library(highcharter)
 
-## Apply Theme
+gh_prep <- "https://github.com/OHI-Science/bhi-1.0-archive/blob/draft/baltic2015/prep"
+gh_layers <- "https://github.com/OHI-Science/bhi-1.0-archive/tree/draft/baltic2015/layers"
+
 thm <- apply_bhi_theme()
 
 
@@ -48,13 +51,37 @@ convertMenuItem <- function(mi, tabName){
 #'
 #' @return
 
-text_links <- function(title = NULL, url = NULL, box_width = 4){
+text_links <- function(title = NULL, url = NULL, box_width = 12){
 
   box(class = "text_link_button",
       h4(a(title, href = url, target = "_blank")),
       width = box_width,
+      height = 90,
       status = "info",
       solidHeader = TRUE)
+}
+
+
+#' print in console pieces to create region menu code
+#'
+#' @param rgn_tab_con
+#'
+#' @return no returned object; prints helpful info in console
+
+make_selectRgn_menu <- function(rgn_tab_con = bhi_db_con){
+
+  rgn <- tbl(rgn_tab_con, "regions") %>%
+    select(region_id, subbasin, region_name) %>%
+    collect() %>%
+    arrange(subbasin) %>%
+    mutate(print_col = sprintf("`%s` = %s", region_name, region_id))
+
+  cat(paste0("`", unique(rgn$subbasin), "` = c(`", unique(rgn$subbasin), "` = )", sep =  "\n"))
+
+  cat("\n\n")
+  for(s in unique(rgn$subbasin)){
+    cat(filter(rgn, subbasin  == s)$print_col, sep = ", \n")
+  }
 }
 
 
@@ -74,11 +101,13 @@ subbasins_shp <- sf::st_read("/Volumes/BHI_share/Shapefiles/HELCOM_subbasins_hol
 # mpa_shp <- sf::st_read()
 
 ## shps combined with score information, only score dim and current assessment year
+message("calculating 'mapping_scores_subbasin' sf object...")
 mapping_scores_subbasin <- make_subbasin_sf(
   subbasins_shp, full_scores_csv,
   goal_code = "all", dim = "score", year = assess_year,
   simplify_level = 1)
 
+message("calculating 'mapping_scores_rgn' sf object...")
 mapping_scores_rgn <- make_rgn_sf(
   rgns_shp, full_scores_csv,
   goal_code = "all", dim = "score", year = assess_year,
