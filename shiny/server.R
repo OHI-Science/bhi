98d7cd11b8343888a36061b9e19c0c866839e793
Eleanore Campbell <eleanorecampbell@eduroam-10-200-61-237.local.su.se>
2019-08-19 16:37:47 +0200

    maps/plots created by modules, for all goals

--- shiny/server.R
+++ shiny/server.R
@@ -6,10 +6,12 @@ function(input, output, session){
   ## WELCOME ----
 
   ## flowerplot
-  callModule(flowerplotCard, "baltic_flowerplot",
-             flower_id = "baltic_flowerplot",
-             dimension = "score",
-             region_id = 0)
+  # callModule(flowerplotCard, "baltic_flowerplot",
+  #            flower_id = "baltic_flowerplot",
+  #            dimension = "score",
+  #            region_id = 0) # region_id_selected = flower_rgn
+  callModule(flowerplotRgnCard, "baltic_flowerplot",
+             region_id_selected = flower_rgn) # region_id_selected = flower_rgn
 
   ## overall index scores map
   # callModule(mapBarplotCard, "index_map_barplot",
@@ -25,7 +27,6 @@ function(input, output, session){
              goal_code = "Index",
              dimension_selected = dimension,
              spatial_unit_selected = spatial_unit)
-
   callModule(mapCard, "index_map",
              goal_code = "Index",
              dimension_selected = dimension,
@@ -52,85 +53,240 @@ function(input, output, session){
              spatial_unit_selected = spatial_unit)
 
 
-
   ## BD ----
   ## Biodiversity
-  # callModule(mapCard, "bd_map",
-  #            goal_code = "BD",
-  #            dimension_selected = dimension,
-  #            spatial_unit_selected = spatial_unit,
-  #            legend_title = "Scores",
-  #            popup_title = "Score:",
-  #            popup_add_field = "Name",
-  #            popup_add_field_title = "Name:")
-  # callModule(barplotCard, "bd_barplot",
-  #            goal_code = "BD",
-  #            dimension_selected = dimension,
-  #            spatial_unit_selected = spatial_unit)
+  callModule(mapCard, "bd_map",
+             goal_code = "BD",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "bd_barplot",
+             goal_code = "BD",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## CS ----
   ## Carbon Storage
+  callModule(mapCard, "cs_map",
+             goal_code = "CS",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "cs_barplot",
+             goal_code = "CS",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## CW ----
   ## Clean Water
+  callModule(mapCard, "cw_map",
+             goal_code = "CW",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "cw_barplot",
+             goal_code = "CW",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## CON ----
   ## Contaminants
+  callModule(mapCard, "con_map",
+             goal_code = "CON",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "con_barplot",
+             goal_code = "CON",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## EUT ----
   ## Eutrophication
+  callModule(mapCard, "eut_map",
+             goal_code = "EUT",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "eut_barplot",
+             goal_code = "EUT",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## TRA ----
   ## Trash
+  callModule(mapCard, "tra_map",
+             goal_code = "TRA",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "tra_barplot",
+             goal_code = "TRA",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## FP ----
   ## Food Provision
+  callModule(mapCard, "fp_map",
+             goal_code = "FP",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "fp_barplot",
+             goal_code = "FP",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## FIS ----
   ## Wild-Caught Fisheries
+  callModule(mapCard, "fis_map",
+             goal_code = "FIS",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "fis_barplot",
+             goal_code = "FIS",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## MAR ----
   ## Mariculture
+  callModule(mapCard, "mar_map",
+             goal_code = "MAR",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "mar_barplot",
+             goal_code = "MAR",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## LE ----
   ## Livelihoods & Economies
+  callModule(mapCard, "le_map",
+             goal_code = "LE",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "le_barplot",
+             goal_code = "LE",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## ECO ----
   ## Economies
+  callModule(mapCard, "eco_map",
+             goal_code = "ECO",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "eco_barplot",
+             goal_code = "ECO",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## LIV ----
   ## Livelihoods
+  callModule(mapCard, "liv_map",
+             goal_code = "LIV",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "liv_barplot",
+             goal_code = "LIV",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## SP ----
   ## Sense of Place
+  callModule(mapCard, "sp_map",
+             goal_code = "SP",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "sp_barplot",
+             goal_code = "SP",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## ICO ----
   ## Iconic Species
+  callModule(mapCard, "ico_map",
+             goal_code = "ICO",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "ico_barplot",
+             goal_code = "ICO",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## LSP ----
   ## Lasting Special Places
-  # callModule(mapCard, "lsp_map",
-  #            goal_code = "LSP",
-  #            dimension_selected = dimension,
-  #            spatial_unit_selected = spatial_unit,
-  #            legend_title = "Scores",
-  #            popup_title = "Score:",
-  #            popup_add_field = "Name",
-  #            popup_add_field_title = "Name:")
+  callModule(mapCard, "lsp_map",
+             goal_code = "LSP",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
   callModule(barplotCard, "lsp_barplot",
              goal_code = "LSP",
              dimension_selected = dimension,
@@ -139,10 +295,34 @@ function(input, output, session){
 
   ## NP ----
   ## Natural Products
+  callModule(mapCard, "np_map",
+             goal_code = "NP",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "np_barplot",
+             goal_code = "NP",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## TR ----
   ## Tourism
+  callModule(mapCard, "tr_map",
+             goal_code = "TR",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit,
+             legend_title = "Scores",
+             popup_title = "Score:",
+             popup_add_field = "Name",
+             popup_add_field_title = "Name:")
+  callModule(barplotCard, "tr_barplot",
+             goal_code = "TR",
+             dimension_selected = dimension,
+             spatial_unit_selected = spatial_unit)
 
 
   ## COMPARE ----
