5a13fb66490772dd0a1cf1dc01060257765f22ad
eleanorecc <eleanorecampbell@ucsb.edu>
2020-01-22 14:10:23 +0100

    some edits

--- R/setup.R
+++ R/setup.R
@@ -14,48 +14,37 @@ subbasin_ids_vec <- 501:517
 
 ## Connections and Directories
 
+## CHANGE BHI ASSESSMENT DIRECTORY HERE!
+dir_assess <- here("baltic")
+
+
 bhi_gh <- "https://github.com/OHI-Science/bhi"
 bhiprep_gh <- "https://github.com/OHI-Science/bhi-prep"
 
-bhi_gh_raw <- stringr::str_replace(
-  bhi_gh,
-  pattern = "github.com",
-  replacement = "raw.githubusercontent.com"
-)
-bhiprep_gh_raw <- stringr::str_replace(
-  bhiprep_gh,
-  pattern = "github.com",
-  replacement = "raw.githubusercontent.com"
-)
+bhi_gh_raw <- stringr::str_replace(bhi_gh, "github.com", "raw.githubusercontent.com")
+bhiprep_gh_raw <- stringr::str_replace(bhiprep_gh, "github.com", "raw.githubusercontent.com")
 
-bhi_gh_api <- stringr::str_replace(
-  bhi_gh,
-  pattern = "github.com",
-  replacement = "api.github.com/repos"
-) %>% paste(
-  "git/trees/master?recursive=1", sep = "/"
-  )
-bhiprep_gh_api <- stringr::str_replace(
-  bhiprep_gh,
-  pattern = "github.com",
-  replacement = "api.github.com/repos"
-) %>% paste(
-  "git/trees/master?recursive=1", sep = "/"
-  )
+bhi_gh_api <- stringr::str_replace(bhi_gh, "github.com", "api.github.com/repos") %>%
+  paste("git/trees/master?recursive=1", sep = "/")
+bhiprep_gh_api <- stringr::str_replace(bhiprep_gh, "github.com", "api.github.com/repos") %>%
+  paste("git/trees/master?recursive=1", sep = "/")
+
 
 bhi_db_file <- "/Users/eleanorecampbell/Desktop/bhi-config.sqlite" # for now... depends on local path to sqlite db...
 bhi_db_con <- DBI::dbConnect(RSQLite::SQLite(), bhi_db_file)
 
-dir_assess <- file.path(here::here(), "baltic") # CHANGE BHI ASSESSMENT DIRECTORY HERE!
-dir_prep <- file.path(dirname(here::here()), "bhi-prep", "prep")
-dir_test <- file.path(dir_assess, "testing")
 
-dir_B <- file.path(c("Darwin" = "/Volumes/BHI_share", # "Windows" = ?
-                     "Linux" = "/home/shares/ohi")[[ Sys.info()[["sysname"]] ]], "BHI 2.0") # CHANGE MAIN AUX BHI DIRECTORY HERE!
-if(Sys.info()[["sysname"]] != "Linux" & !file.exists(dir_B)){ # warning if BHI internal, shared directory doesn't exist
-  paste("The BHI directory dir_share set in R/common.R does not exist.",
-        sprintf("Do you need to mount the BHI server: %s?", dir_B))
+## CHANGE MAIN AUX BHI DIRECTORY HERE!
+dir_B <- file.path(
+  c("Darwin" = "/Volumes/BHI_share",
+    # "Windows" = "?",
+    "Linux" = "/home/shares/ohi")[[ Sys.info()[["sysname"]] ]],
+  "BHI 2.0"
+)
+if(Sys.info()[["sysname"]] != "Linux" & !file.exists(dir_B)){
+  ## warning if BHI shared internal directory doesn't exist
+  paste(
+    "The BHI directory dir_share set in R/common.R does not exist.",
+    sprintf("Do you need to mount the BHI server: %s?", dir_B)
+  )
 }
-
-## Apply theme
-thm <- apply_bhi_theme()
