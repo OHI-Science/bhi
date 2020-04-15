101f2187b45259dd610755aca1cdab25d90114ae
eleanorecc <eleanorecampbell@ucsb.edu>
2020-04-15 17:07:44 +0200

    moving assessment-specific code to calculate_scores.Rmd

--- R/setup.R
+++ R/setup.R
@@ -1,50 +1,36 @@
-## Libraries
+## Libraries ----
 library(here)
-library(tidyverse)
-library(zoo)
+library(readr)
+library(dplyr)
+library(stringr)
+library(tidyr)
 library(ohicore)
-library(httr)
 
+## Global ----
 
-## General
-assess_year <- 2019 # CHANGE BHI ASSESSMENT YEAR HERE!
-rgn_ids_vec <- 1:42
-subbasin_ids_vec <- 501:517
-
-
-## Connections and Directories
-
-## CHANGE BHI ASSESSMENT DIRECTORY HERE!
-dir_assess <- here("baltic")
-
-
+## SET BHI GITHUB URLS HERE
 bhi_gh <- "https://github.com/OHI-Science/bhi"
 bhiprep_gh <- "https://github.com/OHI-Science/bhi-prep"
 
 bhi_gh_raw <- stringr::str_replace(bhi_gh, "github.com", "raw.githubusercontent.com")
+bhi_gh_api <- bhi_gh %>%
+  stringr::str_replace("github.com", "api.github.com/repos") %>%
+  paste("git/trees/master?recursive=1", sep = "/")
+
 bhiprep_gh_raw <- stringr::str_replace(bhiprep_gh, "github.com", "raw.githubusercontent.com")
-
-bhi_gh_api <- stringr::str_replace(bhi_gh, "github.com", "api.github.com/repos") %>%
-  paste("git/trees/master?recursive=1", sep = "/")
-bhiprep_gh_api <- stringr::str_replace(bhiprep_gh, "github.com", "api.github.com/repos") %>%
+bhiprep_gh_api <- bhiprep_gh %>%
+  stringr::str_replace("github.com", "api.github.com/repos") %>%
   paste("git/trees/master?recursive=1", sep = "/")
 
-
-bhi_db_file <- "/Users/eleanorecampbell/Desktop/bhi-config.sqlite" # for now... depends on local path to sqlite db...
-bhi_db_con <- DBI::dbConnect(RSQLite::SQLite(), bhi_db_file)
-
-
-## CHANGE MAIN AUX BHI DIRECTORY HERE!
-dir_B <- file.path(
-  c("Darwin" = "/Volumes/BHI_share",
-    # "Windows" = "?",
-    "Linux" = "/home/shares/ohi")[[ Sys.info()[["sysname"]] ]],
-  "BHI 2.0"
-)
-if(Sys.info()[["sysname"]] != "Linux" & !file.exists(dir_B)){
-  ## warning if BHI shared internal directory doesn't exist
-  paste(
-    "The BHI directory dir_share set in R/common.R does not exist.",
-    sprintf("Do you need to mount the BHI server: %s?", dir_B)
-  )
+## SET MAIN AUX BHI DIRECTORY HERE
+dir_B <- file.path(dirname(here()), "bhi-data", "BHI 2.0")
+if(!file.exists(dir_B)){
+  ## warning if BHI internal, shared directory doesn't exist
+  warning(sprintf(
+    "Shared BHI directory does not exist. Do you need to mount the server:\n%s",
+    dir_B
+  ))
 }
+
+bhi_db_file <- "/Users/eleanorecampbell/Desktop/bhi-config.sqlite" # local path to sqlite db for now...
+bhi_db_con <- DBI::dbConnect(RSQLite::SQLite(), bhi_db_file)
