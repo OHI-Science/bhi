7fedbc408f71cf5934fe75c836dacc322b840a11
eleanorecc <eleanorecampbell@ucsb.edu>
2020-06-29 12:46:27 +0200

    edit to clean water contaminants function
    * saving intermediate indicator layers
    * slight adjustment to penalty formula

--- baltic2019draft/functions/con_2019.R
+++ baltic2019draft/functions/con_2019.R
@@ -26,7 +26,7 @@ CON <- function(layers){
 
     ## STATUS CALCULATIONS ----
     ## calculate status by subbasins
-    result[["basin_status"]] <- layer %>%
+    basin_matrix_status <- layer %>%
       filter(year %in% yrs) %>%
       ## NOTE: CHANGE METHOD FROM BHI1.0
       ## compare to reference point and cap at one before averaging concentrations,
@@ -48,17 +48,30 @@ CON <- function(layers){
         max_dateloc_pts = max(value, na.rm = TRUE),
         min_dateloc_pts = min(value, na.rm = TRUE)
       ) %>%
-      ungroup() %>%
+      ungroup()
+
+    result[["basin_status"]] <- basin_matrix_status %>%
       group_by(subbasin_id) %>%
       summarize(score = mean(status, na.rm = TRUE)) %>% # status from biota & sediment data combined w equal wgt
+      right_join(rgns_complete, by = "subbasin_id") %>%
       mutate(dimension = "status")
+
+    result[["basin_matrix_status"]] <- basin_matrix_status %>%
+      right_join(
+        rgns_complete %>%
+          select(subbasin_id, region_id) %>%
+          mutate(matrix = list(c("bio", "sed"))) %>%
+          tidyr::unnest(c(matrix)),
+        by = c("matrix", "subbasin_id")
+      )
 
 
     ## TREND CALCULATIONS ----
     trendlag = 5 # number of years into future to project trend
     trendyrs <- (max(yrs)-9):max(yrs) # ten years for trend calculation vs five for status
 
-    ## trend approach 4: regression using raw obs, projected value vs most recent yr est., percent change
+
+    ## trend approach: by observations scaled w.r.t. to reference point
     lm_estim <- layer %>%
       filter(year %in% trendyrs) %>%
       ## join rgns_complete info for grouping by subbasins
@@ -72,46 +85,76 @@ CON <- function(layers){
       filter(trendcalc_nyrs >= 6|(trendcalc_nyrs >= 4 & num_obs > 8)) %>%
       do(trend_mdl = lm(value ~ year, data = .)) %>%
       mutate(
-        projected = predict(trend_mdl, data.frame(year = max(trendyrs)+trendlag)),
-        current_estimated = predict(trend_mdl, data.frame(year = max(trendyrs))),
-        trend_score = (projected-current_estimated)/current_estimated
+        health_threshold = ifelse(matrix == "bio", bio_thresh, sed_thresh),
+        ## change w.r.t. health thresholds...
+        expectedchange5yrs = coef(trend_mdl)["year"]*trendlag,
+        trend_score = -expectedchange5yrs/health_threshold
+      ) %>%
+      ungroup()
+
+    result[["basin_matrix_trends"]] <- lm_estim %>%
+      select(-trend_mdl) %>%
+      mutate(subbasin_id = as.numeric(subbasin_id)) %>%
+      right_join(
+        rgns_complete %>%
+          select(subbasin_id, region_id) %>%
+          mutate(matrix = list(c("bio", "sed"))) %>%
+          tidyr::unnest(c(matrix)),
+        by = c("matrix", "subbasin_id")
       )
+
     result[["basin_trend"]] <- lm_estim %>%
-      ungroup() %>%
       select(-trend_mdl) %>%
       group_by(subbasin_id) %>%
       summarize(score = mean(trend_score, na.rm = TRUE) %>% round(3)) %>%
       ## constrain trend values between one and negative one
       mutate(score = ifelse(-1 <= score & score <= 1, score, score*abs(1/score))) %>%
+      right_join(rgns_complete, by = "subbasin_id") %>%
       mutate(dimension = "trend")
 
+
+    ## return scores for bio and sed matrices and combined indicator scores
+    matrix_scores <- dplyr::bind_rows(
+      result$basin_matrix_status %>%
+        mutate(dimension = "status") %>%
+        select(region_id, subbasin_id, score = status, dimension, matrix),
+      result$basin_matrix_trend %>%
+        mutate(dimension = "trend") %>%
+        select(region_id, subbasin_id, score = trend_score, dimension, matrix)
+    )
     indicator <- dplyr::bind_rows(
-      result$basin_status,
-      result$basin_trend
+      result$basin_status %>%
+        select(region_id, subbasin_id, score, dimension) %>%
+        mutate(matrix = "combined"),
+      result$basin_trend %>%
+        select(region_id, subbasin_id, score, dimension) %>%
+        mutate(matrix = "combined")
     )
 
-    return(indicator)
+    return(list(indicator = indicator, matrix_scores = matrix_scores))
   }
 
   yrs <- (scen_year-6):(scen_year-1)
 
   ## three primary contaminants indicators ----
   pcb_indicator <- AlignDataYears(layer_nm="cw_con_pcb", layers_obj=layers) %>%
-    con_indicators(yrs, bio_thresh = 75, sed_thresh = 4.1) %>%
-    dplyr::mutate(indicator = "pcb")
+    rename(year = scenario_year) %>%
+    con_indicators(yrs, bio_thresh = 75, sed_thresh = 4.1)
 
   pfos_indicator <- AlignDataYears(layer_nm="cw_con_pfos", layers_obj=layers) %>%
-    con_indicators(yrs, bio_thresh = 9.1, sed_thresh = NULL) %>%
-    dplyr::mutate(indicator = "pfos")
+    rename(year = scenario_year) %>%
+    con_indicators(yrs, bio_thresh = 9.1, sed_thresh = NULL)
 
   dioxin_indicator <- AlignDataYears(layer_nm="cw_con_dioxin", layers_obj=layers) %>%
-    con_indicators(yrs, bio_thresh = 6.5, sed_thresh = 0.86) %>%
-    dplyr::mutate(indicator = "dioxin")
+    rename(year = scenario_year) %>%
+    con_indicators(yrs, bio_thresh = 6.5, sed_thresh = 0.86)
 
   ## save individual indicators as intermediate results
   for(ind in c("pcb_indicator", "pfos_indicator", "dioxin_indicator")){
     write.table(
-      mutate(get(ind), scen_year = scen_year),
+      bind_rows(get(ind)$indicator, get(ind)$matrix_scores) %>%
+        select(region_id, score, dimension, matrix) %>%
+        mutate(scen_year = scen_year),
       file.path(dir_assess, "intermediate", sprintf("%s.csv", ind)),
       append = file.exists(file.path(dir_assess, "intermediate", sprintf("%s.csv", ind))),
       sep = ",",
@@ -119,6 +162,9 @@ CON <- function(layers){
     )
   }
 
+  pcb_indicator <- dplyr::mutate(pcb_indicator$indicator, indicator = "pcb")
+  pfos_indicator <- dplyr::mutate(pfos_indicator$indicator, indicator = "pfos")
+  dioxin_indicator <- dplyr::mutate(dioxin_indicator$indicator, indicator = "dioxin")
 
   ## join PCB, Dioxin and PFOS indicators, and take average
   cw_con <- dplyr::bind_rows(pcb_indicator, pfos_indicator, dioxin_indicator) %>%
@@ -138,6 +184,7 @@ CON <- function(layers){
 
   ## concerning substances indicator (previously a uniform baltic-wide penalty factor) ----
   concern_subst_layer <- AlignDataYears(layer_nm="cw_con_penalty", layers_obj=layers) %>%
+    rename(year = scenario_year) %>%
     ## look at monitoring only within time period of interest
     dplyr::filter(year %in% yrs) %>%
     mutate(substance = paste0(substance, "_monitored")) %>%
@@ -156,8 +203,10 @@ CON <- function(layers){
     mutate_at(vars(ends_with("_monitored")), list(~ pmin(1, .))) %>%
     ## add columns on number substances and number monitored
     mutate(
-      num_substances = num_substances,
-      num_monitored = rowSums(.[grep("_monitored", names(.))]),
+      ## adding 3 because of the three monitored subtances quantified in the BHI
+      ## (pcbs, pfos, and dioxins)
+      num_substances = num_substances + 3,
+      num_monitored = rowSums(.[grep("_monitored", names(.))]) + 3,
       proportion_monitored = round(num_monitored/num_substances, 2),
       dimension = "status"
     ) %>%
@@ -168,7 +217,7 @@ CON <- function(layers){
     full_join(concern_subst_indicator, by = c("region_id", "dimension")) %>%
     mutate(score_w_penalty = ifelse(dimension == "trend", score, score*proportion_monitored)) %>%
     mutate(goal = "CON") %>%
-    select(region_id, goal, dimension, score)
+    select(region_id, goal, dimension, score = score_w_penalty)
 
   scores <- cw_con_with_penalty
 
