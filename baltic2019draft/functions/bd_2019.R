bbbb87e80fabe30316ff37e3cb4ee0910e26492b
eleanorecc <eleanorecampbell@ucsb.edu>
2020-08-26 11:28:39 +0200

    biodiversity updated approach

--- baltic2019draft/functions/bd_2019.R
+++ baltic2019draft/functions/bd_2019.R
@@ -12,14 +12,11 @@ BD <- function(layers){
 
   scen_year <- layers$data$scenario_year
 
-
   spp_subbasin_assessments <- AlignDataYears(layer_nm="bd_spp_assessments", layers_obj=layers) %>%
     ## scenario year here is the assessment_year_red_list:
     ## because of infrequent iucn assessments, there will inevitably be a lag in biodiversity status...
     rename(year = scenario_year)
 
-  spp_norec <- AlignDataYears(layer_nm="bd_spp_norec", layers_obj=layers) %>%
-    rename(year = scenario_year)
 
   ## Create vulnerability lookup table ----
   ## weights from Halpern et al 2012, SI
@@ -36,7 +33,7 @@ BD <- function(layers){
         ),
         stringsAsFactors = FALSE
       ),
-      wgt = case_when(
+      red_list_category_numeric = case_when(
         red_list_category == "EX - Extinct" ~ 1,
         red_list_category == "RE - Regionally Extinct" ~ 1,
         red_list_category == "CR - Critically Endangered" ~ 0.8,
@@ -47,8 +44,10 @@ BD <- function(layers){
       )
     )
   ## join vulnerability score to subbasin assessments dataframe
-  spp_assess_w_num <- left_join(spp_subbasin_assessments, vuln_lookup, by = "red_list_category")
-
+  ## filter where red_list_category_numeric is NA, as these correspond to unused categories:
+  ## NA - Not Applicable, NE - Not Evaluated, DD - Data Deficient
+  spp_assess_w_num <- left_join(spp_subbasin_assessments, vuln_lookup, by = "red_list_category") %>%
+    filter(!is.na(red_list_category_numeric))
 
 
   ## Calculate Status ----
@@ -57,40 +56,31 @@ BD <- function(layers){
     ## 1:
     ## for each species, take only values from most recent redlist assessment
     ## s.t. the year is less than or equal to the scenario year being evaluated...
-    group_by(scientific_name) %>%
     filter(assessment_year_red_list <= scen_year) %>%
+    group_by(scientific_name) %>%
     filter(assessment_year_red_list == max(assessment_year_red_list)) %>%
     ungroup() %>%
+
     ## 2:
-    ## join info for penalty/scaling based on proportion spp 'unobserved in this decade'...
-    left_join(spp_norec, by = c("region_id", "scientific_name")) %>%
-    group_by(region_id, species_group, wgt, no_rec_this_dec) %>%
-    summarize(nspp = n()) %>%
-    ungroup() %>%
-    mutate(no_observ = ifelse(no_rec_this_dec & wgt > 0 | wgt == 1, nspp, 0)) %>%
-    ## 3:
     ## sum threat weights for each region and taxa (species_group),
     ## normalizing by number distinct spp
-    group_by(region_id, species_group) %>%
+    group_by(region_id, species_group, red_list_category_numeric) %>%
+    summarize(nspp = n()) %>%
+    ungroup() %>%
     rename(weights = red_list_category_numeric) %>%
+    group_by(region_id, species_group) %>%
     summarise(
       wi_spp = sum(weights*nspp)/sum(nspp),
-      nspp = sum(nspp),
-      pct_not_obs = 1 - (sum(nspp) - sum(no_observ))/sum(nspp)
+      nspp = sum(nspp)
     ) %>%
     ungroup() %>%
     mutate(status_taxa_initial = (1 - wi_spp)) %>%
-    ## 4:
-    ## Rescale/penalize using 'spp not observed this decade'
-    ## with 75 percent representing score of zero
-    mutate(status_taxa_penalized = status_taxa_initial*(0.75-pct_not_obs)/0.75) %>%
-    ## 5:
+
+    ## 3:
     ## summarize status values across taxa by region, using geometric mean
     ## normalizing by number disinct species in each taxa
     group_by(region_id) %>%
-    summarize(status = 100*round(exp(weighted.mean(log(status_taxa_penalized), nspp), 2)))
-
-  ## note: penalty has negligible effect on statuses calculated as geometric mean of taxa statuses
+    summarize(status = 100*round(exp(weighted.mean(log(status_taxa_initial), nspp, na.rm = TRUE)), 2))
 
 
   ## Trend ----
