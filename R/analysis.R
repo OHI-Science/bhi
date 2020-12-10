c028d78bc860f9fb0e1387b2f21dc8220a641490
eleanorecc <eleanorecampbell@ucsb.edu>
2020-12-10 17:00:55 +0100

    aggregating subgoals before calculating index scores

--- R/analysis.R
+++ R/analysis.R
@@ -7,30 +7,52 @@ library(ggplot2)
 
 sim_elast_of_sub <- function(scorescsv, calcyear = 2014, elast_of_sub = c(0, 1), niter = 10000, goal_weights = NULL){
 
+  # goal_weights <- data.frame(
+  #   goal = unique(scorescsv$goal),
+  #   weight = rep(1, length(unique(scorescsv$goal)))
+  # )
+
+  ## keep BHI regions 'status' and 'future' dimensions
   scorescsv <- filter(scorescsv, region_id %in% 1:42)
   goalscores <- filter(scorescsv, goal != "Index", dimension %in% c("status", "future"))
   if("year" %in% names(goalscores)){
     goalscores <- filter(goalscores, year == calcyear)
   }
-  ## regions with one or more goal scores of zero will obtain an index value of zero
+  ## regions with one or more goal scores of zero would obtain an index value of zero
   ## to prevent this collapse to zero (looses some useful information), can set zero scores to small value
   ## no other scores have such small scores, so doesnt confuse signal (?)
   goalscores <- mutate(goalscores, score = ifelse(score == 0, 0.1, score))
 
   goalscores <- goalscores %>%
     left_join(goal_weights, by = "goal") %>%
+    ## match subgoals with supragoals, will first aggregate these using generalized mean
+    mutate(supragoal = case_when(
+      goal == "CON"|goal == "EUT"|goal == "TRA" ~ "CW",
+      goal == "FIS"|goal == "MAR" ~ "FP",
+      goal == "ECO"|goal == "LIV" ~ "LE",
+      goal == "ICO"|goal == "LSP" ~ "SP"
+    )) %>%
+    ## for different regions, with different numbers of non-NA goals,
+    ## will have different 'totals'
+    ## excluding NAs?
     mutate(weight = ifelse(is.na(score), NA, weight)) %>%
-    group_by(dimension, region_id) %>%
+    group_by(dimension, region_id, supragoal) %>%
     mutate(rgn_weight = weight/sum(weight, na.rm = TRUE)) %>%
     ungroup()
 
+
   set.seed(10)
-  nsample_uniform <- runif(niter, min = min(elast_of_sub), max = max(elast_of_sub))
+  ## testing exponential distribution
+  # sigmas <- rexp(niter, 2)
+  sigmas <- runif(niter, min = min(elast_of_sub), max = max(elast_of_sub))
+
   ## issues with calculations when sigma gets very small...
   ## remove any sigma values less than 0.0051
-  nsample_uniform <- nsample_uniform[nsample_uniform >= 0.0051]
-  nsample_uniform <- c(modelrun = nsample_uniform)
+  sigmas <- sigmas[sigmas >= 0.0051]
+  sigmas <- c(modelrun = sigmas)
 
+
+  ## calculate scores with varied sigma parameter
   cl <- makeCluster(3)
   registerDoParallel(cl)
 
@@ -40,9 +62,25 @@ sim_elast_of_sub <- function(scorescsv, calcyear = 2014, elast_of_sub = c(0, 1),
     s <- nsample_uniform[[i]]
 
     simulated_scores <- goalscores %>%
+
+      ## aggregating first within subgoals
+      filter(!is.na(supragoal)) %>%
+      group_by(dimension, region_id, supragoal) %>%
+      mutate(weightedscores = rgn_weight*score^((s-1)/s)) %>%
+      summarize(supragoalscores = sum(weightedscores, na.rm = TRUE)^(s/(s-1))) %>%
+      rename(goal = supragoal) %>%
+
+      ## aggregating all goals
+      right_join(goalscores, by = c("region_id", "dimension", "goal")) %>%
+      filter(is.na(supragoal)) %>%
+      mutate(score = ifelse(goal %in% c("CW", "FP", "LE", "SP") & !is.na(score), supragoalscores, score)) %>%
+
+      ## generalized mean calculation for each status and future
       group_by(dimension, region_id) %>%
       mutate(weightedscores = rgn_weight*score^((s-1)/s)) %>%
       summarize(score = sum(weightedscores, na.rm = TRUE)^(s/(s-1))) %>%
+
+      ## score as simple mean of status and future
       group_by(region_id) %>%
       summarize(score = mean(score), dimension = "score") %>%
       arrange(desc(score)) %>%
@@ -67,7 +105,7 @@ sim_elast_of_sub <- function(scorescsv, calcyear = 2014, elast_of_sub = c(0, 1),
   plotdf <- simulated_scores %>%
     mutate(region = as.factor(paste("region", region_id))) %>%
     left_join(
-      read_csv("https://raw.githubusercontent.com/OHI-Science/bhi-prep/master/supplement/lookup_tabs/rgns_complete.csv") %>%
+      readr::read_csv("https://raw.githubusercontent.com/OHI-Science/bhi-prep/master/supplement/lookup_tabs/rgns_complete.csv") %>%
         select(region_id, region_name, region_order),
       by = "region_id"
     )
@@ -101,7 +139,7 @@ sim_elast_of_sub <- function(scorescsv, calcyear = 2014, elast_of_sub = c(0, 1),
 
   plotdf <- simulated_scores %>%
     group_by(region_id) %>%
-    summarise(minrank = min(rank), maxrank = max(rank)) %>%
+    summarise(minrank = min(rank), maxrank = max(rank), meanrank = mean(rank), sd = sd(rank)) %>%
     ungroup() %>%
     left_join(rank_modes, by = "region_id") %>%
     left_join(select(scores_arithmetic, region_id, rank_arithmetic = rank), by = "region_id") %>%
@@ -113,19 +151,24 @@ sim_elast_of_sub <- function(scorescsv, calcyear = 2014, elast_of_sub = c(0, 1),
     mutate(region_name = stringr::str_replace(region_name, ", ", ",\n"))
 
   compareplot <- ggplot(plotdf) +
+    # ggplot(plotdf %>% mutate(meanminussd = meanrank - sd, meanplussd = meanrank + sd)) +
     geom_abline(color = "lightgrey") +
     geom_errorbar(
       aes(x = rank_arithmetic, ymin = minrank, ymax = maxrank),
+      # aes(x = rank_arithmetic, ymin = meanminussd, ymax =  meanplussd),
       width = 0.1, color = "lightsteelblue"
     ) +
     geom_point(aes(x = rank_arithmetic, y = rank_mode)) +
+    # geom_point(aes(x = rank_arithmetic, y = meanrank)) +
     labs(
       x = expression(paste("\nRanking with unlimited substitution posibilities (", sigma, " = ", infinity, ")")),
       y = expression(paste("\nRanking with limited substitution posibilities (", sigma %~% U, "(0, 1))")),
       title = "Position of point along vertical axis represents the rank mode \nand the bar indicates min-max range"
+      # title = "Position of point along vertical axis represents the rank mean \nand the bar indicates one standard deviation around the mean"
     ) +
     ggrepel::geom_text_repel(
       aes(x = rank_arithmetic, y = rank_mode, label = region_name),
+      # aes(x = rank_arithmetic, y = meanrank, label = region_name),
       size = 3.5, color = "dimgrey", segment.colour = NA,
       hjust = 0, vjust = 0
     )
